e6b8be6d201e06a25d8f6438b6db6641ea810837
prakash <prakashk@outlook.com.au>
2023-09-07 23:37:27 +1000

    [feat] Add serverless api url

--- src/app/service/app.service.ts
+++ src/app/service/app.service.ts
@@ -2,12 +2,14 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IFormValue, Vehicle } from '../app.types';
-
+const DEV_API_URL = 'http://localhost:3000';
+const SERVERLESS_API_URL =
+  'https://irfg3ij8l0.execute-api.us-east-1.amazonaws.com/dev';
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  private readonly apiUrl = 'http://localhost:3000';
+  private readonly apiUrl = DEV_API_URL;
   constructor(private http: HttpClient) {}
 
   getMakeAndModels(): Observable<Record<string, string[]>> {
